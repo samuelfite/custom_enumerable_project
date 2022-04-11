4508b5688938a66429a013857639930b7a5f85d0
Sam Fite <scfite98@gmail.com>
2022-04-11 15:32:35 -0400

    finished

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,105 @@
 module Enumerable
   # Your code goes here
+   def my_each_with_index()
+      index = 0
+      while index < self.length do 
+         yield self[index], index
+         index += 1
+      end
+      return self
+   end
+
+   def my_select()
+      ary = []
+      index = 0
+      while index < self.length do 
+         if yield self[index]
+            ary.push(self[index]) 
+         end
+         index += 1
+      end
+      return ary
+   end
+
+   def my_all?()
+      index = 0
+      ret = true
+      while index < self.length do 
+         if not yield self[index]
+            ret = false 
+         end
+         index += 1
+      end
+      return ret
+   end
+
+   def my_any?()
+      index = 0
+      ret = false 
+      while index < self.length do 
+         if yield self[index]
+            ret = true 
+         end
+         index += 1
+      end
+      return ret
+   end
+
+   def my_none?()
+      index = 0
+      ret = true
+      while index < self.length do 
+         if yield self[index]
+            ret = false
+         end
+         index += 1
+      end
+      return ret
+   end
+
+   def my_count()
+      if not block_given?
+         return self.length
+      end
+      index = 0
+      count = 0
+      while index < self.length do 
+         if yield self[index]
+            count += 1
+         end
+         index += 1
+      end
+      return count
+   end
+
+   def my_map()
+      if not block_given?
+         return self
+      end
+      ary = []
+      index = 0
+      while index < self.length do 
+         ary.push( yield self[index] )
+         index += 1
+      end
+      return ary
+   end
+
+   def my_inject(initial=nil)
+      acc = 0
+      if not initial
+         initial = 0
+      else
+         acc = initial
+      end
+      index = 0
+      while index < self.length do 
+         acc = yield acc, self[index]
+         index += 1
+      end
+      return acc
+   end
+
 end
 
 # You will first have to define my_each
@@ -8,4 +108,12 @@ end
 # to this method
 class Array
   # Define my_each here
+   def my_each
+      index = 0
+      while index < self.length do 
+         yield self[index] 
+         index += 1
+      end
+      return self
+   end
 end
